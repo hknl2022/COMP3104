5916e1b7266ae9001cf3d86363a111da4b621b01
hknl2022 <collegehakangbc@gmail.com>
2022-10-08 17:41:30 -0400

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.12cc5d7b57a9eb1e91f0c0dd4d53750b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "466d2db5ab731a860434",
+    "url": "/hknl2022/COMP3104/static/css/main.ddbea115.chunk.css"
+  },
+  {
+    "revision": "466d2db5ab731a860434",
+    "url": "/hknl2022/COMP3104/static/js/main.466d2db5.chunk.js"
+  },
+  {
+    "revision": "00617842e888682fd61f",
+    "url": "/hknl2022/COMP3104/static/js/runtime~main.00617842.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/hknl2022/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "dd4e17671a88be6c76b33d1e79493f14",
+    "url": "/hknl2022/COMP3104/index.html"
+  }
+];
